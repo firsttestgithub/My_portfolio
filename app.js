75bb2d59670d1b1c0405b0acb6dd1db6148bd526
Muhammad Subhan <112649712+firsttestgithub@users.noreply.github.com>
2024-06-30 22:14:49 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+let header = document.getElementById("nav");
+let li = document.querySelector('ul');
+let bar = document.getElementById("bar");
+let cross = document.getElementById('cross1');
+bar.addEventListener('click',()=>{
+cross.style.display="none"
+})
